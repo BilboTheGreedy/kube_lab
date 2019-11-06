43fc0ed1741609828a5d2a51d85ade6b9a43a7f6
Daniel Rapp <daniel.rapp@live.se>
2019-11-06 02:37:17 +0100

    some basic docker stuff

---
+++ docker_stuff/replicate.js
@@ -0,0 +1,13 @@
+config = {
+    "_id" : "rs0",
+    "members" : [
+        {
+            "_id" : 0,
+            "host" : "mongodb01a:27017"
+        },
+        {
+            "_id" : 1,
+            "host" : "mongodb01b:27017"
+        }
+    ]
+}
