2ca96f3a0392feaec2970b84a543c4a0a9c39492
BilboTheGreedy <daniel.rapp@live.se>
2019-11-06 14:37:13 +0100

    Update replicate.js

--- docker_stuff/replicate.js
+++ docker_stuff/replicate.js
@@ -1,13 +1,7 @@
-config = {
-    "_id" : "rs0",
-    "members" : [
-        {
-            "_id" : 0,
-            "host" : "mongodb01a:27017"
-        },
-        {
-            "_id" : 1,
-            "host" : "mongodb01b:27017"
-        }
-    ]
-}
+rs.initiate( {
+  _id : "rs0",
+  members: [
+    { _id: 0, host: "mongodb01a:27017" },
+    { _id: 1, host: "mongodb01b:27017" },
+  ]
+});
